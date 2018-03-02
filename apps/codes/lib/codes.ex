d9b883566223cc1c282140002a38bd13a9edbcb7
Bart Lantz <underhilllabs@gmail.com>
2018-03-02 11:00:17 -0700

    adds codes app to store the agents' codes

---
+++ apps/codes/lib/codes.ex
@@ -0,0 +1,18 @@
+defmodule Codes do
+  @moduledoc """
+  Documentation for Codes.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Codes.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
